a6664aba83accd649b2a0f40f7becbecd7673b9e
Lioudakis Emmanouil <lioudman@gmail.com>
2021-12-22 16:06:04 +0200

    NEW VERSION

--- README.md
+++ README.md
@@ -7,17 +7,18 @@ After solving all the sudokus, using the log file parsing program (file_parser.c
 are calculated and printed on the command prompt.
 
 
-The tests have been performed on Sunday 19 December 2021, using the vesion 1.5 of the interface.
+The tests have been performed on Tuesday 21 December 2021, using the vesion 1.6 of the interface (available at https://github.com/the-rectifier/ace411_sudoku/releases/tag/1.6 ).
 The output log files (easy_out.log, medium_out.log, hard_out.log and ultra_out.log) are placed in the folder OUTPUT folder.
-Additionally, the cmd's output (showing the mean values and standard deviation) is written to the file "cmd_output_19122021.txt".
+Additionally, the cmd's output (showing the mean values and standard deviation) is written to the file "cmd_output_21122021.txt".
+The boards that were solved are placed in the folder easy_sudokus, medium_sudokus, hard_sudokus and ultra_sudokus.
 
 
-Approximately two hours were needed to complete the tests (~25 minutes for the easy boards, ~24 minutes for the medium boards, ~22 minutes for the hard
-boards and ~47 minutes for the ultra boards). For the easy, medium and hard boards, the delay is due to the comminication, while for the ultra boards, the solving
+Approximately 100 minutes were needed to complete the tests (~21 minutes for the easy boards, ~18 minutes for the medium boards, ~18 minutes for the hard
+boards and ~39 minutes for the ultra boards). For the easy, medium and hard boards, the delay is due to the comminication, while for the ultra boards, the solving
 process takes a large portion of the total time.
 
 
 The program supposes that the STK is connected to the COM3 port, else it should be changed in the batch file.
 
 
-All the files of this repository are also submitted on eclass, except from the output log files, because of their large size (~8MB).
+All the files of the "HRY411_Performance_Analysis" repository are also submitted on eclass, except from the output log files, because of their large size (~8MB).
--- perf_analysis.bat
+++ perf_analysis.bat
@@ -8,52 +8,56 @@ DEL /F ultra_out.log
 DEL /F file_parser.exe
 gcc file_parser.c -o file_parser
 
-REM Script to generate and solve 100 sudokus of each difficulty level
+REM Script to solve 100 sudokus of each difficulty level
+REM The sudokus of each level are placed in the corresponding folder
 REM To be used on Windows systems
 REM by elioudakis, Dec. 2021
 
 REM EASY SUDOKU TESTS
-ace411_sudoku_win_v1_5.exe list > easy_out.log 
-FOR /L %%A IN (1,1,100) DO (
-	ECHO NEW GAME %%A >> easy_out.log
-	ace411_sudoku_win_v1_5.exe run -d Easy -r 9600 -u COM3 <batch_input.txt  >> easy_out.log
+ace411_sudoku_win.exe list > easy_out.log 
+FOR %%f IN (easy_sudokus\*) DO (
+	ECHO NEW GAME %%f >> easy_out.log
+	ace411_sudoku_win.exe prog -b %%f -r 9600 -u COM3 -i <batch_input_gen.txt  >> easy_out.log
 )
 
-REM MEDIUM SUDOKU TESTS 
-ace411_sudoku_win_v1_5.exe list > medium_out.log 
-FOR /L %%A IN (1,1,100) DO (
-	ECHO NEW GAME %%A >> medium_out.log
-	ace411_sudoku_win_v1_5.exe run -d Medium -r 9600 -u COM3 <batch_input.txt  >> medium_out.log
-)
-
-REM HARD SUDOKU TESTS
-ace411_sudoku_win_v1_5.exe list > hard_out.log 
-FOR /L %%A IN (1,1,100) DO (
-	ECHO NEW GAME %%A >> hard_out.log
-	ace411_sudoku_win_v1_5.exe run -d Hard -r 9600 -u COM3 <batch_input.txt  >> hard_out.log
-)
-
-REM ULTRA SUDOKU TESTS
-ace411_sudoku_win_v1_5.exe list > ultra_out.log 
-FOR /L %%A IN (1,1,100) DO (
-	ECHO NEW GAME %%A >> ultra_out.log
-	ace411_sudoku_win_v1_5.exe run -d Ultra -r 9600 -u COM3 <batch_input.txt  >> ultra_out.log
-)
- 
- 
-ECHO ON
-
+@ECHO ON
 ECHO EASY LEVEL STATISTICS
 file_parser.exe easy_out.log
+@ECHO OFF
 
+REM MEDIUM SUDOKU TESTS 
+ace411_sudoku_win.exe list > medium_out.log 
+FOR %%f IN (medium_sudokus\*) DO (
+	ECHO NEW GAME %%f >> medium_out.log
+	ace411_sudoku_win.exe prog -b %%f -r 9600 -u COM3 -i <batch_input_gen.txt  >> medium_out.log
+)
+
+@ECHO ON
 ECHO MEDIUM LEVEL STATISTICS
 file_parser.exe medium_out.log
+@ECHO OFF
 
+REM HARD SUDOKU TESTS
+ace411_sudoku_win.exe list > hard_out.log 
+FOR %%f IN (hard_sudokus\*) DO (
+	ECHO NEW GAME %%f >> hard_out.log
+	ace411_sudoku_win.exe prog -b %%f -r 9600 -u COM3 -i <batch_input_gen.txt  >> hard_out.log
+)
+
+@ECHO ON
 ECHO HARD LEVEL STATISTICS
 file_parser.exe hard_out.log
+@ECHO OFF
 
+REM ULTRA SUDOKU TESTS
+ace411_sudoku_win.exe list > ultra_out.log 
+FOR %%f IN (ultra_sudokus\*) DO (
+	ECHO NEW GAME %%f >> ultra_out.log
+	ace411_sudoku_win.exe prog -b %%f -r 9600 -u COM3 -i <batch_input_gen.txt  >> ultra_out.log
+)
+
+@ECHO ON
 ECHO ULTRA LEVEL STATISTICS
 file_parser.exe ultra_out.log
-
 
 PAUSE
